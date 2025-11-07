68f6f3d9ca76cc90066dca3c1f0eeab19374c6e5
EKRAANGEEPHRAIM <ekraangeephraim@gmail.com>
2025-11-07 11:38:44 +0100

    add commit

--- app10.js
+++ app10.js
@@ -1,16 +1,16 @@
 function nameLettres(arr){
 
-var test = arr[1].toLowerCase();
-var cible = arr[0].toLowerCase();
+var test = arr[1].toLowerCase(); // mot dont on veut verifier les lettres
+var cible = arr[0].toLowerCase();// mot dans lequel je cherche
 
 
 for(   i = 0 ; i < test.length ; i++){
     if(cible.indexOf(test[i]) === -1 ) {
-        return false;
+        return false;// une lettre n est pas present --> stop
     }
 }
-return true;
+return true; // toutes les lettres sont presentes
 }
 
 
-console.log(nameLettres(["concupiscence", "sens"]));
+console.log(nameLettres(["concupiscence", "sens"])); // test
